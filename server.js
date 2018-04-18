437c6beb76d76bd2311e9f234b68f78e202a1324
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-04-18 22:55:00 +0530

    heroku after public static

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-  res.status(200).json({
-    message:"ipl scores should code here"
-  })
-})
-console.log("heroku")
+app.use(express.static('public'))
 
 app.listen(port,()=>{
   console.log("listening on port "+port);

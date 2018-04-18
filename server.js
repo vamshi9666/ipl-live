6f61f67314a6fb7788529d02b41ce16b51ad615e
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-04-18 23:22:47 +0530

    process env port

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-// const port = 3000;
-//using process.env
-
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 8080,()=>{
+app.listen(port ,()=>{
   console.log("listening on port "+port);
 })

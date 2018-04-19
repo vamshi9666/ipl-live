6344a29c5e490ef7dbdf4e22c940d532c5405708
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-04-19 09:59:25 +0530

    changed port and host

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.use(express.static('public'))
 
-app.listen(port ,()=>{
+app.listen(server_port, server_host ,()=>{
   console.log("listening on port "+port);
 })

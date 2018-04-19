01233ab6648ae6452cd5aff534748a91020278d1
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-04-19 10:26:18 +0530

    post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.use(express.static('public'))
